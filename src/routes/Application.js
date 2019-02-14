6b6d655db4472b501fee51b1058695a1f692d042
Pablo <pablomf84@gmail.com>
2019-02-14 19:18:42 -0300

    Adding new routes to get applications info and keywords by applications.

--- src/routes/Application.js
+++ src/routes/Application.js
@@ -1,22 +1,33 @@
 
 module.exports = function(app) {
-    var _QUERY_FIND_BY_KEYWORD = "select a.name " +
+    var _QUERY_FIND_BY_KEYWORD = "select a.id, a.name " +
                                  "  from keyword k " +
                                  "          join application_keyword ak on (k.id = ak.id_keyword) " +
                                  "        join application a on (ak.id_application = a.id) " +
                                  " where upper(k.description) LIKE upper( $1 ) || '%'" +
-                                 " group by a.name ";
+                                 " group by a.id, a.name ";
     
-    var _QUERY_FIND_ALL = "select a.name " +
+    var _QUERY_FIND_ALL = "select a.id, a.name " +
                                  "  from keyword k " +
                                  "          join application_keyword ak on (k.id = ak.id_keyword) " +
                                  "        join application a on (ak.id_application = a.id) " +
-                                 " group by a.name ";
+                                 " group by a.id, a.name ";
+
+    var _QUERY_APPLICATION_BY_ID = " select * from application where id = $1 ";
+
+    var _QUERY_FIND_KEYWORD_BY_APPLICATION = "select k.id, k.description " +
+                                 "  from keyword k " +
+                                 "          join application_keyword ak on (k.id = ak.id_keyword) " +
+                                 " where ak.id_application = $1 " +
+                                 " group by k.id, k.description ";
+
+    var _CONNECTION = function(app) {
+        return app.connection.DBConnection.createSingleConnection();
+    }
 
     app.get('/application/v1/findByKeyword/:keyword', function(request, response) {
-        var connection = app.connection.DBConnection.createSingleConnection();
         var keywordParam = request.params.keyword;
-        connection.manyOrNone(_QUERY_FIND_BY_KEYWORD, keywordParam)
+        _CONNECTION(app).manyOrNone(_QUERY_FIND_BY_KEYWORD, keywordParam)
             .then(result => {
                 response.send(result);
             })
@@ -26,9 +37,7 @@ module.exports = function(app) {
     });
 
     app.get('/application/v1/all', function(request, response) {
-        var connection = app.connection.DBConnection.createSingleConnection();
-        var keywordParam = request.params.keyword;
-        connection.manyOrNone(_QUERY_FIND_ALL, null)
+        _CONNECTION(app).manyOrNone(_QUERY_FIND_ALL, null)
             .then(result => {
                 response.send(result);
             })
@@ -36,4 +45,28 @@ module.exports = function(app) {
                 console.log(error);
             });
     });
+
+    app.get('/getApplicationData/:id', function(request, response) {
+        var id = request.params.id;
+        _CONNECTION(app).one(_QUERY_APPLICATION_BY_ID, [id])
+            .then(result => {
+                response.send(result)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    });
+
+    app.get('/getKeywordsByApplication/:id', function(request, response) {
+        var idApplication = request.params.id;
+        _CONNECTION(app).manyOrNone(_QUERY_FIND_KEYWORD_BY_APPLICATION, [idApplication])
+            .then(result => {
+                response.send(result)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    })
+
+
 }
